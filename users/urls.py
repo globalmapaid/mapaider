d22c2669ab691a5783a2f29e9a2401ff47cda269
esemen <ozhan@esemen.com>
2024-12-20 21:46:14 +0000

    Implements DRF auth

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('user', CurrentUserAPIView.as_view(), name='rest_user_details'),
     path('login', SignInAPIView.as_view(), name='rest_login'),
     path('logout', LogoutView.as_view(), name='rest_logout'),
+    path('register/', include('dj_rest_auth.registration.urls')),
 ]
